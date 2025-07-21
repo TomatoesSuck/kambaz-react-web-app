eeda27b2edb58e6864296f6c3a65cb42215b372d
Jiaming Pei <jiamingpei16@gmail.com>
2025-07-20 21:20:45 -0700

    Until 3.5

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,63 @@
+import BooleanVariables from "./BooleanVariables";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputInline";
+import ConditionalOutputInline from "./ConditionalOutputIfElse";
+import LegacyFunctions from "./LegacyFunctions"
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./TodoItem";
+import TodoList from "./TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+
+
 export default function Lab3() {
     return (
       <div id="wd-lab2">
         <h2>Lab 3</h2>
+        <VariablesAndConstants/>
+        <VariableTypes/>
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+        <SimpleArrays/>
+        <ArrayIndexAndLength/>
+        <AddingAndRemovingToFromArrays/>
+        <ForLoops/>
+        <MapFunction/>
+        <FindFunction/>
+        <FindIndex/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoItem/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
       </div>
     );
   }
