c4829d9b1f549a1599a2917f4615ee447890a316
Jiaming Pei <jiamingpei16@gmail.com>
2025-07-27 21:41:06 -0700

    Finished Module and Sign in

--- src/Kambaz/Courses/Modules/ModulesControls.tsx
+++ src/Kambaz/Courses/Modules/ModulesControls.tsx
@@ -1,58 +1,69 @@
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import { Button, Dropdown } from "react-bootstrap";
+import ModuleEditor from "./ModuleEditor";
+import { useState } from "react";
 
-export default function ModulesControls() {
- return (
-   <div id="wd-modules-controls" className="text-nowrap">
-     <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn">
-       <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-       Module
-     </Button>
+export default function ModulesControls(
+  { moduleName, setModuleName, addModule }:
+  { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div id="wd-modules-controls" className="text-nowrap">
+      <Button variant="danger" onClick={handleShow} size="lg" className="me-1 float-end" id="wd-add-module-btn">
+        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+        Module
+      </Button>
 
-     <Dropdown className="float-end me-2">
-       <Dropdown.Toggle variant="secondary" size="lg" id="wd-publish-all-btn">
-         <GreenCheckmark /> Publish All
-       </Dropdown.Toggle>
-       <Dropdown.Menu>
-         <Dropdown.Item id="wd-publish-all">
-           <GreenCheckmark /> Publish All
-         </Dropdown.Item>
-         <Dropdown.Item id="wd-publish-all-modules-and-items">
-           <GreenCheckmark /> Publish all modules and items
-           </Dropdown.Item>
-         <Dropdown.Item id="wd-publish-modules-only">
-           <GreenCheckmark /> Publish modules only
-         </Dropdown.Item>
-         {/* Create two more items with IDs wd-unpublish-all-modules-and-items and wd-unpublish-modules-only with
-             labels Unpublish all modules and items and Unpublish modules only */}
-         <Dropdown.Item id="wd-unpublish-all-modules-and-items">
-           Unpublish all modules and items
-         </Dropdown.Item>
-         <Dropdown.Item id="wd-unpublish-modules-only">
-           Unpublish modules only
-         </Dropdown.Item>
-         </Dropdown.Menu>
-     </Dropdown>
-     {/* Implement the View Progress and Collapse All buttons with IDs wd-view-progress and wd-collapse-all */}
-        {/* View Progress button */}
-        <Button
-          id="wd-view-progress"
-          variant="secondary"
-          size="lg"
-          className="me-2 float-end"
-        >
-          View Progress
-        </Button>
+      <Dropdown className="float-end me-2">
+        <Dropdown.Toggle variant="secondary" size="lg" id="wd-publish-all-btn">
+          <GreenCheckmark /> Publish All
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item id="wd-publish-all">
+            <GreenCheckmark /> Publish All
+          </Dropdown.Item>
+          <Dropdown.Item id="wd-publish-all-modules-and-items">
+            <GreenCheckmark /> Publish all modules and items
+            </Dropdown.Item>
+          <Dropdown.Item id="wd-publish-modules-only">
+            <GreenCheckmark /> Publish modules only
+          </Dropdown.Item>
+          {/* Create two more items with IDs wd-unpublish-all-modules-and-items and wd-unpublish-modules-only with
+              labels Unpublish all modules and items and Unpublish modules only */}
+          <Dropdown.Item id="wd-unpublish-all-modules-and-items">
+            Unpublish all modules and items
+          </Dropdown.Item>
+          <Dropdown.Item id="wd-unpublish-modules-only">
+            Unpublish modules only
+          </Dropdown.Item>
+          </Dropdown.Menu>
+      </Dropdown>
+      {/* Implement the View Progress and Collapse All buttons with IDs wd-view-progress and wd-collapse-all */}
+          {/* View Progress button */}
+          <Button
+            id="wd-view-progress"
+            variant="secondary"
+            size="lg"
+            className="me-2 float-end"
+          >
+            View Progress
+          </Button>
 
-        {/* Collapse All button */}
-        <Button
-          id="wd-collapse-all"
-          variant="secondary"
-          size="lg"
-          className="float-end"
-        >
-          Collapse All
-        </Button>
-   </div>
+          {/* Collapse All button */}
+          <Button
+            id="wd-collapse-all"
+            variant="secondary"
+            size="lg"
+            className="float-end"
+          >
+            Collapse All
+          </Button>
+
+
+          <ModuleEditor show={show} handleClose={handleClose} dialogTitle="Add Module"
+            moduleName={moduleName} setModuleName={setModuleName} addModule={addModule} />
+    </div>
 );}
--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -5,15 +5,25 @@ import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import * as db from "../../Database";
-
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FormControl } from "react-bootstrap";
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Modules() {
   const { cid } = useParams();
-  const modules = db.modules;
+  const [moduleName, setModuleName] = useState("");
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
 
   return (
         <div>
-          <ModulesControls />
+          <ModulesControls  setModuleName={setModuleName} moduleName={moduleName}
+          addModule={() => {
+            dispatch(addModule({ name: moduleName, course: cid }));
+            setModuleName("");}}/>
           <br />
           <br />
           <br />
@@ -25,8 +35,24 @@ export default function Modules() {
                 <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
                   <div className="wd-title p-3 ps-2 bg-secondary">
                     <BsGripVertical className="me-2 fs-3" />
-                    {module.name}
-                    <ModuleControlButtons />
+                    {!module.editing && module.name}
+                    { module.editing && (
+                      <FormControl className="w-50 d-inline-block"
+                            onChange={(e) => 
+                              dispatch(updateModule({ ...module, name: e.target.value })                              )
+                              }
+                            onKeyDown={(e) => {
+                              if (e.key === "Enter") {
+                                dispatch(updateModule({ ...module, editing: false }));
+                              }
+                            }}
+                            defaultValue={module.name}/>
+                    )}
+
+                    <ModuleControlButtons        
+                        moduleId={module._id}
+                        deleteModule={(moduleId) => dispatch(deleteModule(moduleId))}
+                        editModule={(moduleId) => dispatch(editModule(moduleId))}/>
                   </div>
 
                   {module.lessons && (
